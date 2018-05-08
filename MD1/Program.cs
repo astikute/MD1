ec422457e3762dec5b1cf565405755c9466a4f32
astikute <agnese.stikute@gmail.com>
2018-05-08 14:48:47 +0300

    MD1
    Funkcijas: 2 int tipa mainīgo saskaitīšana un rezultāta izvade.

---
+++ MD1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rezultats = Aprekins();
+            Izvade(rezultats);
+            Console.Read();
+        }
+
+        static int Aprekins()
+        {
+            Console.Write("Ievadiet pirmo skaitli: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ievadiet otro skaitli: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            return a + b;
+        }
+
+        static void Izvade(int vertiba)
+        {
+            Console.WriteLine("Summa: " + vertiba);
+        }
+
+    }
+}
